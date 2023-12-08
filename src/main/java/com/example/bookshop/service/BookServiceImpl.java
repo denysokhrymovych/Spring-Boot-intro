c7fa92778147689a4520108ebf078bebc5e8d2d6
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-12-08 11:42:29 +0200

    create model, repository, service, add the logic to BookShopApplication, add properties

---
+++ src/main/java/com/example/bookshop/service/BookServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.bookshop.service;
+
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
