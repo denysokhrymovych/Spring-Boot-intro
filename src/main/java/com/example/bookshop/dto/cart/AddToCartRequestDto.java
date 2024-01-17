03ee080d1c642465fd639b5d01617973789e4f64
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2024-01-17 17:06:36 +0200

    add @NotNull annotation, avoid FetchType.EAGER

--- src/main/java/com/example/bookshop/dto/cart/AddToCartRequestDto.java
+++ src/main/java/com/example/bookshop/dto/cart/AddToCartRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.bookshop.dto.cart;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddToCartRequestDto {
+    @NotNull(message = "Book id cannot be null")
     @Positive(message = "Book id must be greater than 0")
     private Long bookId;
     @Positive(message = "Quantity must be greater than 0")
